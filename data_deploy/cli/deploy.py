c8f805cd5902efdd7a1b0f0f74fdb7bada03dee6
Sebastiaan Alvarez Rodriguez van Ravensberg <sebastiaanalva@gmail.com>
2021-06-01 17:28:11 +0200

    WIP: Deployment main

--- data_deploy/cli/deploy.py
+++ data_deploy/cli/deploy.py
@@ -8,14 +8,15 @@ def subparser(subparsers):
     '''Register subparser modules'''
     deployparser = subparsers.add_parser('deploy', help='deploy data on a cluster.')
     deployparser.add_argument('plugin', metavar='name', type=str, help='Plugin to use for deployment. Use "data-deploy plugin" to list available plugins.')
+    deployparser.add_argument('args', metavar='args', nargs='+', help='Arguments for the plugin. Use "data-deploy <plugin_name> -h" to see possible arguments.')
+    
     # deployparser.add_argument('paths', metavar='paths', nargs='+', help='Data to transmit to the remote. Pointed locations can be files or directories. Separate locations using spaces.')
     # deployparser.add_argument('--dest', metavar='path', type=str, default=defaults.remote_dir(), required=True, help='Destination path on host (default={}). Any non-existing directories on the remote will be created.'.format(defaults.remote_dir()))
     
     # deployparser.add_argument('--admin', metavar='id', dest='admin_id', type=int, default=None, help='ID of the node that will be the primary or admin node, for strategies using those.')
-    # deployparser.add_argument('--strategy', metavar='strategy', type=str, default=defaults.strategy().name.lower(), help='Strategy to deploy data (default={}).'.format(defaults.strategy().name.lower()))
     # deployparser.add_argument('--silent', help='If set, less boot output is shown.', action='store_true')
     # deployparser.add_argument('--retries', metavar='amount', type=int, default=defaults.retries(), help='Amount of retries to use for risky operations (default={}).'.format(defaults.retries()))
-    # return [deployparser]
+    return [deployparser]
 
 
 def deploy_args_set(args):
@@ -26,6 +27,7 @@ def deploy_args_set(args):
         `True` if we found arguments used by this subsubparser, `False` otherwise.'''
     return args.command == 'deploy'
 
+
 def deploy(parsers, args):
     reservation = _cli_util.read_reservation_cli()
-    return deploy(reservation, key_path=args.key_path, admin_id=args.admin_id, paths=args.paths, dest=args.dest, strategy=args.strategy, silent=args.silent, retries=args.retries) if reservation else False
+    return deploy(reservation, plugin=args.plugin, args=args.args) if reservation else False
